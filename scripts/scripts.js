f27c7b3a56fcfed5325ee6c75266da03ef882f89
kjdhl <90534103+kjdhl@users.noreply.github.com>
2022-10-03 22:08:27 -0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+function reajustarSalario(){
+    let salario = parseFloat(document.querySelector("#salarioEntrada").value);
+    //console.log(typeof(salario));
+    
+    let string = ["Salário antes do ajuste: R$ ","Percentual de aumento aplicado: ","Valor do aumento: R$ ","Novo Salário, após aumento: R$ "];
+
+    if(salario<=280){
+        let reajuste = salario*0.2
+        resposta.innerHTML = `${string[0]}${salario}\n${string[1]}20% \n${string[2]}${reajuste}\n${string[3]}${salario+reajuste}`
+    }
+    else if(salario<700){
+        let reajuste = salario*0.15
+        resposta.innerHTML = `${string[0]}${salario}\n${string[1]}15% \n${string[2]}${reajuste}\n${string[3]}${salario+reajuste}`
+    }
+    else if(salario<1500){
+        let reajuste = salario*0.1
+        resposta.innerHTML = `${string[0]}${salario}\n${string[1]}10% \n${string[2]}${reajuste}\n${string[3]}${salario+reajuste}`
+    }
+    else if(salario>=1500){
+        let reajuste = salario*0.05
+        resposta.innerHTML = `${string[0]}${salario}\n${string[1]}5% \n${string[2]}${reajuste}\n${string[3]}${salario+reajuste}`
+    }
+    
+    //resposta.innerHTML = salario;
+}
+
+
+
